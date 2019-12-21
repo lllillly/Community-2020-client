336cca301f41b76edf45cce4b4bda42c3f9ac752
qkdgywls0503 <qkdgywls0503@naver.com>
2019-12-21 15:32:35 +0900

    mixins settings, dotenv, pug

--- Desktop/4leaf-education/blog-project/src/server.js
+++ Desktop/4leaf-education/blog-project/src/server.js
@@ -1,13 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
 
-const app = express();
+const app = express()
+
+dotenv.config();
+
+
+// 1. 확장자를 .pug로 검색한다.
+// 2. "views" 폴더를 default 경로로 자동 인식한다.
+app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-    res.send("Hello Node");
-});
+    res.render("home");
+})
 
-app.get("/gh", (req, res) => {
-    res.send("This page is GH Page!");
-});
+app.get("/post", (req, res) => {
+    res.render("post");
+})
 
-app.listen(3000, () => console.log("server.start"));
+app.listen(process.env.PORT, () => console.log("server.start"));
